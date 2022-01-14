5c8c62c44ba37a9da55b647b1816d2e2c8f16339
argjend551 <argjend551@hotmail.com>
2022-01-14 09:35:01 +0100

    Lade till en till sida favorites

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Home from './pages/Home';
-
+import Home from "./pages/Home";
+import Favorites from "./pages/Favorites";
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/" element={<Home/>} />
+      </Routes>
+      <Routes>
+        <Route path="/favorites" element={<Favorites/>} />
       </Routes>
     </BrowserRouter>
   );
