a8d996205969cf9a7f6d501e43f569bd0c9f085a
Tim <tim.hogklint@gmail.com>
2022-01-14 09:34:34 +0100

    Header compnent in App

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,9 @@ import Home from './pages/Home';
 
 function App() {
   return (
+
     <BrowserRouter>
+      <Header />
       <Routes>
         <Route path="/" element={<Home />} />
       </Routes>
