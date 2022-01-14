560f0c3820fdeb083730dde9979abcbd95634d2d
Eorelynne <89979161+Eorelynne@users.noreply.github.com>
2022-01-14 09:55:52 +0100

    Added footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Home from './pages/Home';
+import Home from "./pages/Home";
 import Header from "./Header";
 import Favorites from "./pages/Favorites";
+import Footer from "./Footer";
 
 function App() {
   return (
-
     <BrowserRouter>
       <Header />
       <Routes>
@@ -15,6 +15,7 @@ function App() {
       <Routes>
         <Route path="/favorites" element={<Favorites />} />
       </Routes>
+      <Footer />
     </BrowserRouter>
   );
 }
