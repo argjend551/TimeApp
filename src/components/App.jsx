296f34957a511ade895e9b08181591a6b5b1fb59
Patlund <patrik.ojerssonlundgren@gmail.com>
2022-01-17 14:18:12 +0100

    Changed the img link in JSON to a local img. Changed the import for readcities.jsx in App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,7 +4,7 @@ import Home from "../pages/Home";
 import Header from "./Header";
 import Favorites from "../pages/Favorites";
 import Footer from "./Footer";
-import ReadCityList from "./components/ReadCityList";
+import ReadCityList from "./ReadCityList";
 
 function App() {
   return (
