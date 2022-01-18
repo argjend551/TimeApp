58c300000a23616026974ff7c3da921c2f05de7c
Eorelynne <89979161+Eorelynne@users.noreply.github.com>
2022-01-18 11:26:29 +0100

    Additions to DetailedCityPage

--- src/pages/DetailedCityPage.jsx
+++ src/pages/DetailedCityPage.jsx
@@ -2,10 +2,21 @@ import React from "react";
 
 function DetailedCityPage() {
   return (
-    <div>
+    <div
+      className="cityBackgroundImg"
+      style={{
+        backgroundImage: "url(images/citybackground/beijingbackground.jpg)",
+      }}
+    >
       <h1>Detailed City Page</h1>
       <h2>Analog Clock</h2>
       <h2>Digital Clock</h2>
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Hic facere
+        perferendis, illo eius in repellat tenetur fugiat, quod vero, cupiditate
+        repudiandae esse error amet aut magnam voluptatem cum tempore
+        voluptates.
+      </p>
     </div>
   );
 }
