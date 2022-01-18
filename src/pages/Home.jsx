d167d9d0876a9dcdd2c138daeaab48659ef96709
Patlund <patrik.ojerssonlundgren@gmail.com>
2022-01-18 10:45:11 +0100

    Removed unused import statements.

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import ReadCityList from "../components/ReadCityList";
 import Row from 'react-bootstrap/Row'
 import Container from 'react-bootstrap/Container'
-import Col from 'react-bootstrap/Col'
 
 function Home() {
   return (
