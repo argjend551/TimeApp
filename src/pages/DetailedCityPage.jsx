a5cd4602a962da977334189a8de8924fca58c1c1
Eorelynne <89979161+Eorelynne@users.noreply.github.com>
2022-01-18 10:35:18 +0100

    Commiting json-file again

---
+++ src/pages/DetailedCityPage.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function DetailedCityPage() {
+  return (
+    <div>
+      <h1>Detailed City Page</h1>
+      <h2>Analog Clock</h2>
+      <h2>Digital Clock</h2>
+    </div>
+  );
+}
+
+export default DetailedCityPage;
