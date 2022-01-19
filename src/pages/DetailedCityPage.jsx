a487ccb0b2815005339cab349c69ad06c63a1df6
Eorelynne <89979161+Eorelynne@users.noreply.github.com>
2022-01-19 12:16:10 +0100

    Changed styling in detailedCityPage

--- src/pages/DetailedCityPage.jsx
+++ src/pages/DetailedCityPage.jsx
@@ -16,15 +16,20 @@ function DetailedCityPage() {
         backgroundImage: `url(${imageBgr})`,
       }}
     >
-      <h1>{city}</h1>
-      <h2>Analog Clock</h2>
-      <GetLocalTime offset={timezone} settime={settime} />
-      <p>
-        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Hic facere
-        perferendis, illo eius in repellat tenetur fugiat, quod vero, cupiditate
-        repudiandae esse error amet aut magnam voluptatem cum tempore
-        voluptates.
-      </p>
+      <h1 className="cityName">{city}</h1>
+      <div className="citypagecontent">
+        <h2>Analog Clock</h2>
+        <GetLocalTime offset={timezone} settime={settime} />
+        <div className="aboutCity">
+          <h3 className="titleAboutCity">About {city}</h3>
+          <p>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Hic facere
+            perferendis, illo eius in repellat tenetur fugiat, quod vero,
+            cupiditate repudiandae esse error amet aut magnam voluptatem cum
+            tempore voluptates.
+          </p>
+        </div>
+      </div>
     </div>
   );
 }
