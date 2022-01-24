810170eb0c8784c085bd0d1b57babeb9afa7bc38
Eorelynne <89979161+Eorelynne@users.noreply.github.com>
2022-01-24 09:43:16 +0100

    Small changes in styling in detailcitypage

--- src/components/RenderCityDetails.jsx
+++ src/components/RenderCityDetails.jsx
@@ -27,15 +27,6 @@ function RenderCityDetails(props) {
             {" "}
             <GetLocalTime offset={timezone} settime={settime} />
           </div>
-          <div className="aboutCity">
-            <h3 className="titleAboutCity">About {city}</h3>
-            <p>
-              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Hic
-              facere perferendis, illo eius in repellat tenetur fugiat, quod
-              vero, cupiditate repudiandae esse error amet aut magnam voluptatem
-              cum tempore voluptates.
-            </p>
-          </div>
         </div>
       </div>
     </>
