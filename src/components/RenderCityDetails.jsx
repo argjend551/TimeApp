e252777d5c698a2f4c3d234efb4caee089e16907
Eorelynne <89979161+Eorelynne@users.noreply.github.com>
2022-01-25 09:07:57 +0100

    Added toggle to clocks on city page

--- src/components/RenderCityDetails.jsx
+++ src/components/RenderCityDetails.jsx
@@ -7,6 +7,7 @@ function RenderCityDetails(props) {
   const { town } = props;
   const { id, city, timezone, image, imageBgr } = town;
   const [time, settime] = useState();
+  const [showAnalog, setShowAnalog] = useState(true);
 
   //Scroll to top of page when component mounts the first time.
   useEffect(() => {
@@ -23,10 +24,26 @@ function RenderCityDetails(props) {
       >
         <h1 className="cityName">{city}</h1>
         <div className="citypagecontent">
-          <Clock offset={timezone} settime={settime} />
-          <div className="digitalClockCityPage">
-            {" "}
-            <GetLocalTime offset={timezone} settime={settime} />
+          {!!showAnalog && <Clock offset={timezone} settime={settime} />}
+          {!showAnalog && (
+            <div className="digitalClockCityPage">
+              <GetLocalTime offset={timezone} settime={settime} />
+            </div>
+          )}
+          <div>
+            {!!showAnalog && (
+              <button
+                className="btnToggle"
+                onClick={() => setShowAnalog(false)}
+              >
+                Show Digital Clock
+              </button>
+            )}
+            {!showAnalog && (
+              <button className="btnToggle" onClick={() => setShowAnalog(true)}>
+                Show Analog Clock
+              </button>
+            )}
           </div>
         </div>
       </div>
