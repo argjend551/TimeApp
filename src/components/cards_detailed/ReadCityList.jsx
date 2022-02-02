c2e9251f8873c5dbfab57b3312790a795260f0bd
Patlund <patrik.ojerssonlundgren@gmail.com>
2022-02-02 09:00:58 +0100

    Added comments and removed non used imports.

--- src/components/cards_detailed/ReadCityList.jsx
+++ src/components/cards_detailed/ReadCityList.jsx
@@ -4,6 +4,10 @@ import Row from "react-bootstrap/Row";
 function ReadCityList(props) {
   const { cities } = props;
 
+  //Mapping the Cities Array with the info from the cities.JSON, the JSON info is read at home.jsx.
+  //<Readcity /> adds the code and design for the cards.
+
+
   return (
     <Row>
       {cities.map(town => (
